603daeddaa41a2596d9e19559840eb0c795cedb7
Roshan Singh <rosh.s568@gmail.com>
2023-09-11 18:45:25 +0530

    Previous linting error fixed

--- lib/source.py
+++ lib/source.py
@@ -5,7 +5,3 @@ def wiki(name="War Goddess", length=1):
     """Searches for a Wikipedia article and returns a summary."""
     my_wiki = wikipedia.summary(name, length)
     return my_wiki
-
-a = 5;
-a = a;
-print(a)
