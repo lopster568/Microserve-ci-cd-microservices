47852e24565f00dfc199e3755be60686d980e241
Roshan Singh <rosh.s568@gmail.com>
2023-09-11 18:43:18 +0530

    Testing to fail the linting

--- lib/source.py
+++ lib/source.py
@@ -5,3 +5,7 @@ def wiki(name="War Goddess", length=1):
     """Searches for a Wikipedia article and returns a summary."""
     my_wiki = wikipedia.summary(name, length)
     return my_wiki
+
+a = 5;
+a = a;
+print(a)
